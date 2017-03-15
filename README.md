580b5433b030f48531d42fee365884c38db18198
Riku Walve <riku.walve@gmail.com>
2017-03-15 17:13:51 +0200

    add licence and HTSlib submodule

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Extract
 
-Extracts reads from BAM alignments based on regions.
+Standalone tool for filtering reads based on paired-end read alignments.
+
+## Compilation
+
+CMake and a C++11 compiler, such as g++ 4.5 or newer are required.
+Depends on HTSlib and GATB-core, both of which are included as git submodules.
+
+Compile HTSlib according to their instructions.
+GATB-core is compiled as needed by default.
+
+  mkdir build; cd build; cmake ..; make
+
+## Usage
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
-// Copyright 2016 Riku Walve
+/*****************************************************************************
+ *  Extract
+ *  Copyright (C) Riku Walve 2017
+ *
+ *  Contact: riku.walve@cs.helsinki.fi
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
 
 #include <extract.hpp>
 
